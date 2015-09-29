0cd595f1827cc150dbbfce16598568a3a0b120ca
Analcr1996 <anacollantes@yahoo.com>
2015-09-29 18:16:46 -0400

    changed backgrounds and color of dog...

--- x1.java
+++ x1.java
@@ -1,14 +1,21 @@
-//////// What am I trying to do???
-//////// What is my name?  (CST 112; today's date?)
+//////// Exercise x2:  modularize exercise x1, and add dog to chase hero.
+//////// Whoever Whoever  (CST 112; today's date?)
+
+//////// Please change these to your name and today's date.
+String author=  "Ana Collantes";
+String title=  " Timmy being chased by a dog. ";
+String help=  " Click to relocate hero \n 'q' to quit; 'r' to reset. ";
+
 
 //// GLOBALS:  coordinates, speed, etc.
-float x, y;       // Position of creature.
-float dx, dy;     // Speed.
 float horizon;
+float x, y;       // Position.
+float dx, dy;     // Speed.
+float dogX, dogY;
 
 //// SETUP:  window size, initialization (start in middle of screen).
 void setup() {
-  size( 540,300);
+    size( 540,300);
   horizon=  height/4;
   x=  width/2;
   y=  height/2;
@@ -18,7 +25,15 @@ void setup() {
 
 //// NEXT FRAME:  scene, action, show.
 void draw() {
-  //// SCENE:  sky, sun, tree, house, etc.
+  scene();
+  hero();
+  dog();
+  messages();
+}
+
+//// SCENE:  sky, sun, tree, house, etc.
+void scene() {  
+  
   background( 100,200,260 );                // sky
   fill( 255,255,0 );
   ellipse( width*3/3.2, height/23, 25, 25 );    // sun
@@ -81,49 +96,51 @@ fill(4,103);
 arc(130, 190, 10, 10, PI, TWO_PI);
 
 
-
-
-  fill(0);
-  text( "My name is Annie", 10,height-20 );                                          
-                                            
-  //// ACTION:  move (x,y) coordinates.
-  x=  x + dx;
-  y=  y + dy;
-  
-
-  
-  //// SHOW:  display the creature at (x,y)
-
-  /* INSERT YOUR CODE HERE! */
-  fill(255,0,0); rect( x,y, 30,50 );        /* REPLACE THIS WITH YOUR OWN CODE! */
-
-  text( "Tim", x,y );
-
+ /* INSERT YOUR CODE HERE! */    
+  /* REPLACE THIS STUB! */  text( "Scene 1", 100, 100 );
 }
+
+void messages() {
+  text( title, width/3, 20 );
+  text( help, width*2/3, 30 );
+  text( author, 10,height-20 );
+}
+
+//// ACTION:  move (x,y) coordinates of hero & dog; show them.
+void hero() {
+  /* INSERT YOUR CODE HERE! */
+  /* REPLACE THIS STUB! */  text( "[[[[ Nicky ]]]]", 200, 200 );
+  rect( x,y, 50,80 );
+}
+void dog() {
+  dogX=  dogX - (dogX-x)/30;
+  dogY=  dogY - (dogY-y)/40;
+  text( dogX, 10, 10 );
+  text( dogY, 10, 20 );
+  //
+  fill( 118,112,73 );
+  rect(dogX,dogY, 60,30 );
+  /* INSERT YOUR CODE HERE! */
+  /* REPLACE THIS STUB! */  text( "woof, woof!!", 150, 150 );
+}
+
+
+
 
 
 //////// HANDLERS:  mouse clicks, keys
 void mousePressed() {
   x=  mouseX;                             // Set (x,y) to mouse
   y=  mouseY;
- 
-
-  
-  dx=  random( -4, +5 );                  // random speed.
-  dy=  random( -2, +3 );
-
+  //
+  dx=  random( -6, +6 );                  // random speed.
+  dy=  random( -4, +4 );
 }
-
-
 
 void keyPressed() {
   if (key == 'q') {
-    exit();    // press 'q' key to QUIT.
- 
+    exit();                           // press 'q' key to QUIT.
   }
-}
-   
-   
-
-   
+  /* INSERT YOUR CODE HERE! */
+}  
 
